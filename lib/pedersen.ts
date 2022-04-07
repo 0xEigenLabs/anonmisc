9d8bba5068c52e9fc5bc4517982833130d196fee
JangoCCC <zebron@ieigen.com>
2022-04-07 18:55:39 +0800

    save progress

--- lib/pedersen.ts
+++ lib/pedersen.ts
@@ -1,3 +1,105 @@
+import * as crypto from 'crypto'
+import { buildBabyjub } from 'circomlibjs';
+let babyjub
+let G
+
+async function generateRandom() {
+    babyjub = await buildBabyjub(); 
+    let random;
+    do {
+        random = BigInt("0x" + crypto.randomBytes(32).toString('hex'));
+    } while (random >= babyjub.order); // make sure it's in the safe range
+    return random;
+}
+
+async function generateH() {
+    babyjub = await buildBabyjub(); 
+    G = babyjub.Generator
+    return babyjub.mulPointEscalar(G, generateRandom())
+}
+
+var H = generateH()
+
+// commit to a Value X
+//   r - private Key used as blinding factor
+//   H - shared private? point on the curve
+async function commitTo(H, r, x) {
+    babyjub = await buildBabyjub(); 
+    G = babyjub.Generator
+    console.log(typeof r)
+    console.log(typeof babyjub.order)
+    return babyjub.addPoint(babyjub.mulPointEscalar(G, r % babyjub.order), babyjub.mulPointEscalar(H, x))
+}
+
+// sum two commitments using homomorphic encryption
+//
+async function add(Cx, Cy) {
+    babyjub = await buildBabyjub(); 
+    return babyjub.addPoint(Cx, Cy);
+}
+
+// subtract two commitments using homomorphic encryption
+//
+async function sub(Cx, Cy) {
+    babyjub = await buildBabyjub(); 
+    return babyjub.addPoint(Cx, -Cy);
+}
+
+// add two known values with blinding factors
+//   and compute the committed value
+//   add rX + rY (blinding factor private keys)
+//   add vX + vY (hidden values)
+async function addCommitment(H, rX, rY, vX, vY) {
+    // umod to wrap around if negative
+    babyjub = await buildBabyjub(); 
+    G = babyjub.Generator
+    var rZ = (rX + rY) % babyjub.order;
+    return babyjub.addPoint(babyjub.mulPointEscalar(G, rZ), babyjub.mulPointEscalar(H, (vX + vY) % babyjub.order));
+}
+
+// subtract two known values with blinding factors
+//   and compute the committed value
+//   add rX - rY (blinding factor private keys)
+//   add vX - vY (hidden values)
+async function subCommitment(H, rX, rY, vX, vY) {
+    babyjub = await buildBabyjub(); 
+    G = babyjub.Generator
+    var rZ;
+    if (rX > rY) {
+        rZ = (rX - rY) % babyjub.order;
+    } else {
+        rZ = (rX - rY) + babyjub.order;
+    }
+    return babyjub.addPoint(babyjub.mulPointEscalar(G, rZ), babyjub.mulPointEscalar(H, (vX - vY) % babyjub.order));
+}
+
+/**
+ * Verifies that the commitment given is the same
+ * 
+ * @param {*} H - secondary point
+ * @param {*} C - commitment
+ * @param {*} r - blinding factor private key used to create the commitment
+ * @param {*} v - original value committed to
+ */
+ async function verify(H, C, r, v) {
+    babyjub = await buildBabyjub(); 
+    G = babyjub.Generator
+    let res = babyjub.addPoint(babyjub.mulPointEscalar(G, r % babyjub.order), babyjub.mulPointEscalar(H, v));
+    return babyjub.F.eq(res[0], C[0]) && babyjub.F.eq(res[1], C[1]);
+}
+
+module.exports = {
+    commitTo,
+    add,
+    sub,
+    addCommitment,
+    subCommitment,
+    verify,
+    generateRandom,
+    generateH
+}
+
+/*
 import * as crypto from 'crypto';
 
 const secp256k1 = require('@noble/secp256k1')
@@ -64,25 +166,14 @@ function subCommitment(H, rX, rY, vX, vY) {
     return G.multiply(rZ).add(H.multiply((vX - vY) % CURVE.n));
 }
 
-/**
  * Verifies that the commitment given is the same
  * 
  * @param {*} H - secondary point
  * @param {*} C - commitment
  * @param {*} r - blinding factor private key used to create the commitment
  * @param {*} v - original value committed to
- */
 function verify(H, C, r, v) {
     return G.multiply(r % CURVE.n).add(H.multiply(v)).equals(C);
 }
 
-module.exports = {
-    commitTo,
-    add,
-    sub,
-    addCommitment,
-    subCommitment,
-    verify,
-    generateRandom,
-    generateH
-}
+ */
--- test/pedersen.test.ts
+++ test/pedersen.test.ts
@@ -1,65 +1,67 @@
 import {expect} from "chai";
+import buildBabyjub from "circomlibjs";
 
 var pedersen = require('../lib/pedersen');
 
 // pre-generate a bunch of blinding factors
 var r = Array(10).fill(null).map((x, i) => i).map(i => pedersen.generateRandom());
+console.log(r)
 
 // this is another point of the curve that will be used
 //   as the generate points for the hidden values
 var H = pedersen.generateH();
 
-describe('pedersen', () => {
+describe.only('pedersen babyjub', () => {
 
-    it('should commit to a sum of two values', () => {
+    it('should commit to a sum of two values', async() => {
 
         //transfer amount - we want to transfer 5 tokens    
-        var tC = pedersen.commitTo(H, r[1], 5n);
+        var tC = await pedersen.commitTo(H, r[1], 5n);
 
         // Alice 10 - 5 = 5
-        var aC1 = pedersen.commitTo(H, r[2], 10n);
-        var aC2 = pedersen.sub(aC1, tC);
+        var aC1 = await pedersen.commitTo(H, r[2], 10n);
+        var aC2 = await pedersen.sub(aC1, tC);
 
         // bob 7 + 5 (aC2) = 12
-        var bC1 = pedersen.commitTo(H, r[4], 7n);
-        var bC2 = pedersen.add(bC1, tC);
+        var bC1 = await pedersen.commitTo(H, r[4], 7n);
+        var bC2 = await pedersen.add(bC1, tC);
 
         // alice's balance to go down by 5
         // aC1 - tC = aC2
-        var checkAC2 = pedersen.subCommitment(H, r[2], r[1], 10n, 5n);
+        var checkAC2 = await pedersen.subCommitment(H, r[2], r[1], 10n, 5n);
         expect(aC2.equals(checkAC2)).to.eq(true);
 
         // bob's balance to go up by 5
         // bC1 + tC = bC2 
-        var checkBC2 = pedersen.addCommitment(H, r[4], r[1], 7n, 5n);
+        var checkBC2 = await pedersen.addCommitment(H, r[4], r[1], 7n, 5n);
 
         expect(bC2.equals(checkBC2)).to.eq(true);
         
         // verify the commitment
-        expect(pedersen.verify(H, bC2, r[4] + r[1], 7n + 5n)).to.eq(true);
+        expect(await pedersen.verify(H, bC2, r[4] + r[1], 7n + 5n)).to.eq(true);
     });
 
-    it('should fail if not using the correct blinding factors', () => {
+    it('should fail if not using the correct blinding factors', async() => {
         //transfer amount - we want to transfer 5 tokens
-        var tC = pedersen.commitTo(H, r[1], 5n);
+        var tC = await pedersen.commitTo(H, r[1], 5n);
 
         // Alice 10 - 5 = 5
-        var aC1 = pedersen.commitTo(H, r[2], 10n);
-        var aC2 = pedersen.sub(aC1, tC);
+        var aC1 = await pedersen.commitTo(H, r[2], 10n);
+        var aC2 = await pedersen.sub(aC1, tC);
 
         // bob 7 + 5 (aC2) = 12
-        var bC1 = pedersen.commitTo(H, r[4], 7n);
-        var bC2 = pedersen.add(bC1, tC);
+        var bC1 = await pedersen.commitTo(H, r[4], 7n);
+        var bC2 = await pedersen.add(bC1, tC);
 
         // now to check
         // r[0] -> is not the correct blinding factor
-        var checkAC2 = pedersen.subCommitment(H, r[0], r[1], 10n, 5n);
+        var checkAC2 = await pedersen.subCommitment(H, r[0], r[1], 10n, 5n);
 
         expect(aC2.equals(checkAC2)).to.eq(false);
         
         // now to check
         // r[0] -> is not the correct blinding factor
-        var checkBC2 = pedersen.addCommitment(H, r[0], r[1], 7n, 5n);
+        var checkBC2 = await pedersen.addCommitment(H, r[0], r[1], 7n, 5n);
 
         expect(bC2.equals(checkBC2)).to.eq(false);
     })
